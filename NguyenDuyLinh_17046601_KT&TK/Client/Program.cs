efc687432bc149cce775545eb631d962a9778029
duylinh289 <54764171+duylinh289@users.noreply.github.com>
2020-05-10 09:23:36 +0700

    Nộp bài

---
+++ NguyenDuyLinh_17046601_KT&TK/Client/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient();
+            client.Connect("127.0.0.28", 5000);
+            Stream stream = client.GetStream();
+            Console.WriteLine("Connected!");
+            Console.WriteLine("Enter what you want to send:");
+            string str = Console.ReadLine();
+
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            byte[] data = encoding.GetBytes(str);
+            stream.Write(data, 0, data.Length);
+
+            data = new byte[1024];
+            stream.Read(data, 0, 1024);
+            Console.WriteLine(encoding.GetString(data));
+
+            stream.Close();
+            client.Close();
+            Console.Read();
+        }
+    }
+}
---
+++ NguyenDuyLinh_17046601_KT&TK/Server/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress IP = IPAddress.Parse("127.0.0.28");
+            TcpListener listener = new TcpListener(IP, 5000);
+
+            listener.Start();
+            Console.WriteLine("Server started on " + listener.LocalEndpoint);
+            Console.WriteLine("Waiting for a connection...");
+            Socket socket = listener.AcceptSocket();
+            Console.WriteLine("Connection received from " + socket.RemoteEndPoint);
+
+            byte[] data = new byte[1024];
+            socket.Receive(data);
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            string str = encoding.GetString(data);
+
+            socket.Send(encoding.GetBytes("Received: " + str));
+
+            socket.Close();
+            listener.Stop();
+            Console.Read();
+        }
+    }
+}
